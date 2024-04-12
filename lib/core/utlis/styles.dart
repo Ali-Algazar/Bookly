5dff8755ec08e101413457ce2ef560e76c5ab255
algazar <ilamohamed12@gmail.com>
2024-04-12 14:22:32 +0200

    create font style class

---
+++ lib/core/utlis/styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/rendering.dart';
+
+abstract class FontStyles {
+  static TextStyle fontStyles18 = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle fontStyles20 = const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'GT-Sectra',
+  );
+  static TextStyle fontStyles14 = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle fontStyles15 = const TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle fontStyles16 = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle fontStyles30 = const TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'GT-Sectra',
+  );
+}
