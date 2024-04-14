d52419acba0c74b53e52ada5bb67322b9258a74e
algazar <ilamohamed12@gmail.com>
2024-04-14 14:10:17 +0200

    finish ui

--- lib/features/Home/presentation/views/widget/Book_info_body.dart
+++ lib/features/Home/presentation/views/widget/Book_info_body.dart
@@ -14,36 +14,88 @@ class BookInfoBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          const CustomBookInfoAppBer(),
-          const BookCaverImage(),
-          const SizedBox(
-            height: 30,
-          ),
-          Text(
-            'The Jungle Book',
-            style: FontStyles.fontStyles30,
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            'Rudyard Kipling',
-            style: FontStyles.fontStyles18.copyWith(
-              fontWeight: FontWeight.w500,
-              color: Colors.white.withOpacity(0.70),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CustomBookInfoAppBer(),
+            const BookCaverImage(),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'The Jungle Book',
+              style: FontStyles.fontStyles30,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              'Rudyard Kipling',
+              style: FontStyles.fontStyles18.copyWith(
+                fontWeight: FontWeight.w500,
+                color: Colors.white.withOpacity(0.70),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Rating(),
+            const SizedBox(
+              height: 30,
+            ),
+            const CustomDownlodeButtom(),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  'You can also like',
+                  style: FontStyles.fontStyles14.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const SimilarBooksListView(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, bottom: 8),
+      child: SizedBox(
+        height: 114,
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.only(right: 4),
+            child: Container(
+              height: 114,
+              width: 71,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage(testCover),
+                fit: BoxFit.fill,
+              )),
             ),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Rating(),
-          const SizedBox(
-            height: 30,
-          ),
-          const CustomDownlodeButtom(),
-        ],
+        ),
       ),
     );
   }
